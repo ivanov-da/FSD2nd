63a1528a44f237894408050502fc029b369a4d8e
Dmitriy <idmitriy.fsd@gmail.com>
2020-04-16 19:04:03 +0300

    started pagination

---
+++ src/components/pagination/pagination.js
@@ -0,0 +1,30 @@
+function simpleTemplating(data) {
+  var html = '<ul>';
+  $.each(data, function (index, item) {
+    html += '<li>' + item + '</li>';
+  });
+  html += '</ul>';
+  return html;
+}
+
+$(document).ready(function () {
+  $('#pagination-container').pagination({
+    dataSource: function (done) {
+      var result = [];
+      for (var i = 1; i < 180; i++) {
+        result.push(i);
+      }
+      done(result);
+    },
+    //dataSource: [1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7],
+    pageSize: 12,
+    autoHidePrevious: true,
+    autoHideNext: true,
+    pageRange: 1,
+    callback: function (data, pagination) {
+      var html = simpleTemplating(data);
+      $('#data-container').html(html);
+    }
+  })
+  console.log('pagination lol')
+});
