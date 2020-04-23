76b7ce5d43d79e749da6f038c97a3c6b55addc7f
Dmitriy <idmitriy.fsd@gmail.com>
2020-04-23 18:10:51 +0300

    continue dropdown
    styled dropdown

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,3 +1,6 @@
 $(document).ready(() => {
   $('.iqdropdown').iqDropdown();
+  $('<p class="button-decrement-minus">-</p>').replaceAll('.button-decrement .icon-decrement');
+  $('<p class="button-increment-plus">+</p>').replaceAll('.button-increment .icon-increment');
+
 });
