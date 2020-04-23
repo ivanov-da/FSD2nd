cbc01baee8a6de0936ba58fe0235d179249081a6
Dmitriy <idmitriy.fsd@gmail.com>
2020-04-23 14:35:35 +0300

    started dropdwn, styled input and menu

--- src/pages/jquery-for-parcel.js
+++ src/pages/jquery-for-parcel.js
@@ -3,3 +3,4 @@ window.$ = window.jQuery = jquery;
 require('../../node_modules/jquery.maskedinput/src/jquery.maskedinput.js');
 require('../../node_modules/paginationjs/dist/pagination.js');
 require('../../node_modules/ion-rangeslider/js/ion.rangeSlider.min.js');
+require('../../node_modules/item-quantity-dropdown/lib/item-quantity-dropdown.min.js');
