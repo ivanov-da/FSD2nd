2f4be8c905dff8bfcf2baab593b57fa3dc5e9060
Dmitriy <idmitriy.fsd@gmail.com>
2020-04-30 18:57:23 +0300

    continue date-dropdown
    added apply button

--- src/components/date-dropdown/date-dropdown.js
+++ src/components/date-dropdown/date-dropdown.js
@@ -1,29 +1,27 @@
 $(document).ready(() => {
-  $('.js-date-dropdown').datepicker({
+  $(".js-date-dropdown").datepicker({
     clearButton: true,
     range: true,
-    multipleDatesSeparator: ' - ',
+    multipleDatesSeparator: " - ",
     clearButton: true,
     //inline: true,
-    prevHtml: '<div class="date-dropdown__arrows material-icons">arrow_back</div>',
-    nextHtml: '<div class="date-dropdown__arrows material-icons">arrow_forward</div>',
+    prevHtml:
+      '<div class="date-dropdown__arrows material-icons">arrow_back</div>',
+    nextHtml:
+      '<div class="date-dropdown__arrows material-icons">arrow_forward</div>',
     navTitles: {
-      days: 'MM yyyy',
+      days: "MM yyyy",
     },
-    onShow: function (inst, animationCompleted) {
-      if (!animationCompleted) {
-        console.log(inst);
-        inst.$datepicker.find('.datepicker--button').after("<span class=\"datepicker--button datepicker--button-apply\" data-action=\"apply\">Применить</span>");
-      }
-    },
+  });
 
-    onHide: function (inst, animationCompleted) {
-      if (animationCompleted) {
-        console.log(inst);
-        inst.$datepicker.find('.datepicker--button-apply').remove();
+  let $dpr = $(".js-date-dropdown").data("datepicker");
+  let $applyButton = $dpr.$datepicker
+    .find(".datepicker--button")
+    .after(
+      '<span class="datepicker--button datepicker--button-apply" data-action="apply">Применить</span>'
+    );
 
-      }
-    },
-
+  $(".datepicker--button-apply").click(() => {
+    $dpr.hide();
   });
 });
