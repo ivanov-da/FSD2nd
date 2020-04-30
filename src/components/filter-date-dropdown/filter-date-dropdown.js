c38b8327116b06331fea570ff615a3cf36a85115
Dmitriy <idmitriy.fsd@gmail.com>
2020-04-30 19:53:23 +0300

    started filter-date-dropdown, based on date-dropdown

---
+++ src/components/filter-date-dropdown/filter-date-dropdown.js
@@ -0,0 +1,27 @@
+$(document).ready(() => {
+  $(".js-date-dropdown").datepicker({
+    clearButton: true,
+    range: true,
+    multipleDatesSeparator: " - ",
+    clearButton: true,
+    //inline: true,
+    prevHtml:
+      '<div class="date-dropdown__arrows material-icons">arrow_back</div>',
+    nextHtml:
+      '<div class="date-dropdown__arrows material-icons">arrow_forward</div>',
+    navTitles: {
+      days: "MM yyyy",
+    },
+  });
+
+  let $dpr = $(".js-date-dropdown").data("datepicker");
+  let $applyButton = $dpr.$datepicker
+    .find(".datepicker--button")
+    .after(
+      '<span class="datepicker--button datepicker--button-apply" data-action="apply">Применить</span>'
+    );
+
+  $(".datepicker--button-apply").click(() => {
+    $dpr.hide();
+  });
+});
--- src/pages/form-elements.js
+++ src/pages/form-elements.js
@@ -5,3 +5,4 @@ import '../components/slider/slider.js';
 import '../components/exp-checkbox/exp-checkbox.js';
 import '../components/dropdown/dropdown.js';
 import '../components/date-dropdown/date-dropdown.js';
+import '../components/filter-date-dropdown/filter-date-dropdown.js';
