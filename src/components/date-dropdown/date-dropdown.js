654d61c48ca99a25257fcdd6fec5a3488d34adaa
Dmitriy <idmitriy.fsd@gmail.com>
2020-04-30 14:43:47 +0300

    continue date-dropdown
    added apply button

--- src/components/date-dropdown/date-dropdown.js
+++ src/components/date-dropdown/date-dropdown.js
@@ -4,11 +4,26 @@ $(document).ready(() => {
     range: true,
     multipleDatesSeparator: ' - ',
     clearButton: true,
-    inline: true,
+    //inline: true,
     prevHtml: '<div class="date-dropdown__arrows material-icons">arrow_back</div>',
     nextHtml: '<div class="date-dropdown__arrows material-icons">arrow_forward</div>',
     navTitles: {
       days: 'MM yyyy',
     },
+    onShow: function (inst, animationCompleted) {
+      if (!animationCompleted) {
+        console.log(inst);
+        inst.$datepicker.find('.datepicker--button').after("<span class=\"datepicker--button datepicker--button-apply\" data-action=\"apply\">Применить</span>");
+      }
+    },
+
+    onHide: function (inst, animationCompleted) {
+      if (animationCompleted) {
+        console.log(inst);
+        inst.$datepicker.find('.datepicker--button-apply').remove();
+
+      }
+    },
+
   });
 });
