d97734f69085df47d69248617e01c4fd73dae933
Dmitriy <idmitriy.fsd@gmail.com>
2020-05-01 13:43:11 +0300

    added folder to form elements page, ref block links for correct work

--- src/pages/form-elements.js
+++
@@ -1,8 +0,0 @@
-import '../components/like-button/like-button.js';
-import '../components/pagination/pagination.js';
-import '../components/text-field/text-field.js';
-import '../components/slider/slider.js';
-import '../components/exp-checkbox/exp-checkbox.js';
-import '../components/dropdown/dropdown.js';
-import '../components/date-dropdown/date-dropdown.js';
-import '../components/filter-date-dropdown/filter-date-dropdown.js';
