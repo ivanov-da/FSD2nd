f3ac8ef3c64e2d5c167dafe09ecb59ccff2d2c8c
Dmitriy <idmitriy.fsd@gmail.com>
2020-05-22 14:52:46 +0300

    continue search room page
    added content for sidebar

--- src/pages/search-room/search-room.js
+++ src/pages/search-room/search-room.js
@@ -1,1 +1,6 @@
 import '../../components/header/header.js';
+import '../../components/pagination/pagination.js';
+import '../../components/slider/slider.js';
+import '../../components/exp-checkbox/exp-checkbox.js';
+import '../../components/dropdown/dropdown.js';
+import '../../components/filter-date-dropdown/filter-date-dropdown.js';
