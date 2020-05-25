b63b9fa881c9399008633762060bc0e94a019f3d
Dmitriy <idmitriy.fsd@gmail.com>
2020-05-25 15:25:47 +0300

    added donut chart

--- src/components/donut-chart/donut-chart.js
+++ src/components/donut-chart/donut-chart.js
@@ -1,38 +1,39 @@
 $(document).ready(function () {
-  let ctx = document.getElementById("myChart").getContext("2d");
-
-  let data = {
-    datasets: [{
-      data: [10, 20, 30]
-    }],
-    labels: [
-      'Red',
-      'Yellow',
-      'Blue'
-    ]
+  var options = {
+    // legend: false,
+    responsive: false
   };
-
-  var htmlData = [{
-      value: 90,
-      color: "#74cfae"
-    },
-    {
-      value: 10,
-      color: "#f2f2f2"
-    }
-  ];
-
-
-  let options = {
-
-  };
-
-  var myChart = new Chart(ctx, {
+  new Chart($("#canvas1"), {
     type: 'doughnut',
-    data: htmlData,
-    options: {
-      percentageInnerCutout: 80
+    tooltipFillColor: "rgba(51, 51, 51, 0.55)",
+    data: {
+      labels: [
+        "Удовлетворительно",
+        "Хорошо",
+        "Великолепно",
+        "Разочарован"
+      ],
+      datasets: [{
+        data: [65, 65, 130, 0],
+        backgroundColor: [
+          "#BC9CFF",
+          "#6FCF97",
+          "#FFE39C",
+          "#919191"
+        ],
+        hoverBackgroundColor: [
+          "#8BA4F9",
+          "#66D2EA",
+          "#FFBA9C",
+          "#3D4975"
+        ]
+      }]
     },
+    options: {
+      responsive: false,
+      cutoutPercentage: 90,
+      legend: false
+    }
   });
 
 
